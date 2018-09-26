a25648b7e26aeaeea92ccd7387ef2771188a2edb
Timmy93 <federico.seri@mail.polimi.it>
2018-09-26 11:33:00 +0200

    Added arduino codes

---
+++ CapacityMonitoringModule/mkr_completo4_deepsleep.ino
@@ -0,0 +1,502 @@
+/*
+ * TODO:
+ *  - Set the topic of height and weight as: id/height and id/weight
+ *  - Setup the deep sleep
+ *  - Change server and wifi
+ */
+#include <WiFi101.h>
+#include <PubSubClient.h>
+#include <HX711_ADC.h>
+#include <FlashStorage.h>
+#include <FlashAsEEPROM.h>
+#include <RTCZero.h>
+
+//MKR1000 pin
+#define D0    0 //TRIGGER - White
+#define D1    1 //ECHO - Violet
+#define D2    2
+#define D3    3
+#define D4    4 //CLK - Green
+#define D5    5 //DAT - Blue
+#define D6    6
+#define D7    7
+
+//Wifi 
+#define UNI 1
+//Is this a testing sketch?
+#define TESTING 0
+//Are you using iot wifi line?
+#define IOT_WIFI 1
+
+#define DEEP_SLEEP true
+
+#if TESTING == 1
+  //Define the time that passes before next sampling
+  #define SLEEPHH 0
+  #define SLEEPMIN 0
+  #define SLEEPSEC 25
+  //Threshold to send the measure
+  #define THR_HEIGHT 2 //Difference in percentage befere send new measure
+  #define THR_MIN_HEIGHT 1 //The min number of cm that must change to ensure that the height has changed
+  #define THR_WEIGHT 5 //Difference in grams befere send new measure
+  #define MAX_TIME_NO_SEND 1 //Maximum number of iteration without sending a message
+#else
+  #define SLEEPHH 1
+  #define SLEEPMIN 0
+  #define SLEEPSEC 0
+  #define THR_HEIGHT 5 //Difference in percentage befere send new measure
+  #define THR_MIN_HEIGHT 1 //The min number of cm that must change to ensure that the height has changed
+  #define THR_WEIGHT 5 //Difference in grams befere send new measure
+  //Send at least every hour
+  #define MAX_TIME_NO_SEND 6
+#endif
+
+#if IOT_WIFI == 0
+  //MQTT info
+  #define mqtt_server "93.50.134.152"
+  #define mqtt_user "smartbin"
+  #define mqtt_password "test123"
+
+  #if UNI == 1
+    //Wifi lab
+    #define wifi_ssid "wlan_saltuaria"
+    #define wifi_password "antlabpolitecnicomilano"
+  #else
+    //Wifi casa
+    #define wifi_ssid "Yeah science, bitch!"
+    #define wifi_password "@Ste@Fed@Sil@"
+  #endif
+#else
+  //MQTT info
+  #define mqtt_server "10.172.0.11"
+  #define mqtt_user ""
+  #define mqtt_password ""
+
+  #define wifi_ssid "IoTPolimi"
+  #define wifi_password "ZpvYs=gT-p3DK3wb"
+#endif
+
+
+#define weight_topic "/weight"
+#define height_topic "/height"
+
+//Bin fixed data
+#define STABILISING_TIME 3000
+#define OFFSET 135292655
+#define SCALE_FACTOR -206.53
+
+//Instantiate cell
+HX711_ADC LoadCell(D5, D4);
+//MQTT client
+WiFiClient espClient;
+PubSubClient mqtt_client(espClient);
+//Timer
+RTCZero rtc;
+
+/* Change these values to set the current initial time */
+const byte seconds = 0;
+const byte minutes = 00;
+const byte hours = 9;
+
+/* Change these values to set the current initial date */
+const byte day = 31;
+const byte month = 5;
+const byte year = 18;
+
+//Values
+long height = 2;
+long weight = 3;
+int successive_boot = -1;
+long old_height = -100;
+long old_weight = -100;
+int passed_cycles = 0;
+unsigned long id = 0;
+
+//Tare value
+long bin_height;
+long offset;
+byte mac[6];
+
+FlashStorage(first_boot_storage, int);
+FlashStorage(height_tare_storage, long);
+FlashStorage(weight_tare_storage, long);
+
+void setup() {
+  delay(5000);
+  if ( ! TESTING ) {
+    Serial.println("Unplug before starting calibration");
+    delay(3000);
+  }
+  
+  Serial.println("Start tare");
+  //Tare everything at startup
+  tare();
+  //get id
+  id = getId();
+  Serial.println("ID: "+String(id));
+
+  
+  Serial.println("Setting timer");
+  rtc.begin();
+  rtc.setTime(hours, minutes, seconds); //set time
+  rtc.setDate(day, month, year); //set date
+}
+
+
+void loop() {
+  Serial.println("_____START_____");
+
+  action();
+
+  if (successive_boot == 1) {
+    Serial.println("This was not first boot");
+  } else if (successive_boot == 0) {
+    Serial.println("First boot!");
+  } else {
+    Serial.println("Storage problem");
+  }
+  delayFunction();
+}
+
+
+
+/*
+   Do a tare only if this is the first boot
+*/
+void tare() {
+  //Initialize load cell
+  LoadCell.powerUp();
+  LoadCell.begin();
+  //Check this is the first boot
+  successive_boot = first_boot_storage.read();
+  if (successive_boot == 1) {
+    //Load old values from memory
+    bin_height = height_tare_storage.read();
+    offset = weight_tare_storage.read();
+    LoadCell.startUsingOldTare(STABILISING_TIME, offset);
+  } else if (successive_boot == 0) {
+    //Tare and store values to memory
+    Serial.println("Storing bin height");
+    bin_height = tare_height();
+    height_tare_storage.write(bin_height);
+    Serial.println("Storing bin weight");
+    LoadCell.start(STABILISING_TIME);
+    offset = LoadCell.getTareOffset();
+    weight_tare_storage.write(offset);
+
+    first_boot_storage.write(1);
+  } else {
+    Serial.print("Unexpected value as successive_boot: ");
+    Serial.println(successive_boot);
+  }
+
+  //Finish set-up loadcell calibration
+  LoadCell.setCalFactor(SCALE_FACTOR);
+  LoadCell.powerDown();
+}
+
+
+
+void action() {
+  //Get new data
+  float temp_h = getHeightAIO();
+  Serial.print("Altezza misurata: ");
+  Serial.println(temp_h);
+  height = bin_height - temp_h;
+  Serial.print("Height: ");
+  Serial.print(height);
+  Serial.print("cm su ");
+  Serial.println(bin_height);
+  weight = getWeightAIO();
+  Serial.print("Weight: ");
+  Serial.println(weight);
+
+  //Compare data
+  if (changed_weight() || changed_height() || elapsed_max_time()) {
+    Serial.println("Threshold passed, send data!");
+    //Update old_values only after a message
+    old_height = height;
+    old_weight = weight;
+    passed_cycles = 0;
+    //Send data to server
+    sendData();
+  } else {
+    Serial.println("No send");
+  }
+  //increment the number ofX cycles passed
+  passed_cycles += 1;
+}
+
+//Tells if the weight threshold is passed
+bool changed_weight() {
+  float difference = weight - old_weight;
+  difference = abs(difference);
+
+  return difference > THR_WEIGHT;
+}
+
+//Tells if the height threshold is passed
+bool changed_height() {
+  float difference = 100 * (height - old_height);
+  difference = abs(difference);
+  float height_thr = THR_HEIGHT * bin_height;
+  return (difference > height_thr) && THR_MIN_HEIGHT * 100 < difference;
+}
+
+//Tells if the max number of iteration without a message is passed
+bool elapsed_max_time() {
+  return passed_cycles >= MAX_TIME_NO_SEND;
+}
+
+//Tells if the lid was open during the measure
+bool closed_lid() {
+  return height + THR_MIN_HEIGHT >= 0;
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  long start1 = millis();
+  long start2 = millis();
+  long elapsed = millis() - start1;
+  while (WiFi.status() != WL_CONNECTED) {
+    elapsed = millis() - start2;
+    if (elapsed > 500) {
+      start2 = millis();
+      Serial.print(".");
+    }
+    if (start2 - start1 > 5000) {
+      Serial.println("Retry connection");
+      start1 = start2;
+      WiFi.disconnect();
+      WiFi.begin(wifi_ssid, wifi_password);
+    }
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+  Serial.println("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void sendData() {
+  bool published = false;
+  char buf[256];
+  String temp;
+  Serial.print("Sending data...");
+  String weight_str = "{ \"weight\":" + String(weight) + " }";
+  String height_str =  "{ \"height\":" + String(height) + ", \"max_height\":" + String(bin_height) + " }";
+
+  setup_wifi();
+  Serial.println("Connesso");
+  mqtt_client.setServer(mqtt_server, 1883);
+  Serial.println("Server settato");
+  if (!mqtt_client.connected()) {
+    Serial.println("Reconnect...");
+    reconnect();
+  }
+  mqtt_client.loop();
+  Serial.println("Invio i dati");
+  //Send the values
+  Serial.print("New weight:");
+  Serial.println(weight_str);
+  temp = String(id)+weight_topic;
+  temp.toCharArray(buf, 256);
+  mqtt_client.publish(buf, weight_str.c_str(), true);
+  Serial.print("New height:");
+  Serial.println(height_str);
+  if (closed_lid()) {
+    temp = String(id)+height_topic;
+    temp.toCharArray(buf, 256);
+    published = mqtt_client.publish(buf, height_str.c_str(), true);
+  }
+  if (published) {
+    Serial.println("Published");
+  } else {
+    Serial.println("Not published");
+  }
+
+  //Enter in low power mode
+  mqtt_client.disconnect();
+  espClient.stop();
+  WiFi.disconnect();
+  WiFi.end();
+  return;
+}
+
+void reconnect() {
+  // Loop until we're reconnected
+  bool mqtt_connected = false;
+  Serial.print("Check connection");
+  while (!mqtt_client.connected()) {
+    if (WiFi.status() != WL_CONNECTED) {
+      WiFi.disconnect();
+      setup_wifi();
+    }
+
+    Serial.print("Attempting MQTT connection...");
+    if (mqtt_user == "") {
+     // mqtt_connected = mqtt_client.connect("MKR1000Client");
+      Serial.println("Simple connection - No user and psw" + String(mqtt_connected));
+    } else {
+      mqtt_connected = mqtt_client.connect("MKR1000Client", mqtt_user, mqtt_password);
+    }
+    if (mqtt_client.connect("MKR1000Client")) {
+      Serial.println("connected");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(mqtt_client.state());
+      Serial.println(" try again in 5 seconds");
+      // Wait 5 seconds before retrying
+      delay(2000);
+    }
+  }
+}
+
+long getHeightAIO() {
+  //Inizialize pin
+  pinMode(D0, OUTPUT);
+  //Echo
+  pinMode(D1, INPUT);
+
+  //Capture value
+  long duration, distance;
+  digitalWrite(D0, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(D0, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(D0, LOW);
+  duration = pulseIn(D1, HIGH);
+  distance = (duration / 2) / 29.1;
+  return distance;
+}
+
+long getWeightAIO() {
+  LoadCell.powerUp();
+  LoadCell.begin();
+  long t = millis();
+  int count = 0;
+
+  while (1 == 1) {
+    LoadCell.update();
+    //get smoothed value from data set + current calibration factor
+    if (millis() > t + STABILISING_TIME) {
+      long i = (long) round(LoadCell.getData());
+      count = count + 1;
+      //Serial.print("W: ");
+      //Serial.println(i);
+      t = millis();
+
+      if (count > 0) {
+        count = 0;
+        //Serial.println("Return");
+        LoadCell.powerDown();
+        return i;
+      }
+    }
+  }
+}
+
+/*
+ * Get 16 values of height
+ * Return the mean of the values removing the bigger and the smaller
+ */
+long tare_height() {
+  int samples = 16;
+  float value = 0;
+  long temp;
+  long max;
+  long min;
+  for (int i = 0; i < samples; i++) {
+    temp = getHeightAIO();
+    //calcualte max and min value
+    if (i == 0) {
+      max = temp;
+      min = temp;
+    } else if (max < temp) {
+      max = temp;
+    } else if (min > temp) {
+      min = temp;
+    }
+    //Calculate the sum
+    value += temp;
+    delay(10);
+  }
+  //calculate the mean
+  value = (value - max - min) / (samples - 2);
+  return long(round(value));
+}
+
+/*
+ * Get the unique id of the board using the MAC address
+ */
+unsigned long getId() {
+  setup_wifi();
+  unsigned long id = 0;
+//  printMac();
+//  for (int i = 0; i < 6; i++) {
+//    Serial.print(String(mac[i]));
+  //  Serial.print(":");
+//  }
+  Serial.println("");
+  WiFi.macAddress(mac);
+  const int digits = 3;
+  const int num_mac = 3;
+  unsigned long temp = 0;
+  for (int i = 0; i < num_mac; i++) {
+    unsigned long value = long(mac[i]);
+    temp = value * pow(10, i*digits);
+    //Serial.println(temp);
+    id += temp;
+  }
+  disconnect();
+  return id;
+}
+
+void printMac() {
+  WiFi.macAddress(mac);
+  Serial.print("MAC: ");
+  Serial.print(mac[5],HEX);
+  Serial.print(":");
+  Serial.print(mac[4],HEX);
+  Serial.print(":");
+  Serial.print(mac[3],HEX);
+  Serial.print(":");
+  Serial.print(mac[2],HEX);
+  Serial.print(":");
+  Serial.print(mac[1],HEX);
+  Serial.print(":");
+  Serial.println(mac[0],HEX);
+}
+
+void alarmMatch(){
+  //DO NOTHING - The execution restart from the loop where it was called
+}
+
+void delayFunction() {
+  if (DEEP_SLEEP) {
+    Serial.println("In Deep Sleep");
+    rtc.setTime(0, 0, 00);
+    rtc.setAlarmTime(SLEEPHH, SLEEPMIN, SLEEPSEC);
+    rtc.enableAlarm(rtc.MATCH_HHMMSS); //set alarm
+    rtc.attachInterrupt(alarmMatch); //creates an interrupt that wakes the SAMD21 which is triggered by a FTC alarm
+    rtc.standbyMode(); //library call
+  } else {
+    Serial.println("In IDLE...");
+    delay((SLEEPHH * 60 * 60 + SLEEPMIN * 60 + SLEEPSEC) * 1000);
+  }
+}
+
+void disconnect() {
+  WiFi.disconnect();
+  WiFi.end();
+}
+
---
+++ VisionModule/mkr_camera4_final.ino
@@ -0,0 +1,526 @@
+/*
+   Here I'll try to read the image and then send to the server
+
+*/
+#include <Wire.h>                    // I2C library, used to 
+#include <SPI.h>                     // SPI library, used to control camera
+#include <ArduCAM.h>                 // Camera library, creates a camera object and define image configuration
+#include "memorysaver.h" // File with OV5642's reg value
+#include <WiFi101.h>
+
+//Wifi
+#define UNI 1
+//Is this a testing sketch?
+#define TESTING 1
+//High quality
+#define HIGH_QUALITY 0
+//Enable the use of IoT wifi
+#define IOT_WIFI 1
+//Seconds to wait before making a pic
+#define SECOND_PAUSE 200
+#define TIME_TO_PIC 3
+#define RANGE_MIN 4
+#define RANGE_MAX 25
+
+// set pin 0 as the slave select for the digital pot:
+#define CS_CAM 0
+#define LED_UNSORTED 1
+#define LED_PLASTIC 2
+#define LED_PAPER 3
+#define LED_DISTANCE_OK 4
+#define LED_DISTANCE_NO 5
+#define ECHO_PIN 6
+#define TRIG_PIN 7
+#if IOT_WIFI == 0
+  //MQTT info
+  #define host "smartbin.ddns.net"
+  #define api_page "/image_api.php"
+
+  #if UNI == 1
+    //Wifi lab
+    #define wifi_ssid "wlan_saltuaria"
+    #define wifi_password "antlabpolitecnicomilano"
+  #else
+    //Wifi casa
+    #define wifi_ssid "Yeah science, bitch!"
+    #define wifi_password "@Ste@Fed@Sil@"
+  #endif
+#else
+  //MQTT info
+  #define host "10.172.0.11"
+  #define api_page "/image_api.php"
+  #define wifi_ssid "IoTPolimi"
+  #define wifi_password "ZpvYs=gT-p3DK3wb"
+#endif
+
+
+
+
+#if HIGH_QUALITY == 1
+//It uses more space than 20kb - Partial image
+#define IMAGE_QUALITY OV5642_640x480
+#else
+#define IMAGE_QUALITY OV5642_320x240
+#endif
+
+#define BUFFER 20000
+#define SMALL_BUF 512
+
+//The string before the selected bin
+#define BIN_STRING "BIN: "
+
+//Inizialize cam
+ArduCAM myCam( OV5642, CS_CAM );
+static uint8_t read_fifo_burst(ArduCAM myCAM, String CAM_NAME);
+
+int secondOk = 0;
+bool ledKo_ON = false;
+bool ledOk_ON = false;
+
+//Wifi Client
+WiFiClient client;
+
+void setup() {
+  uint8_t vid, pid;
+  uint8_t temp;
+
+  Wire.begin();
+  delay(3000);
+
+  test_led();
+  
+  Serial.println(F("ACK CMD ArduCAM Start!"));
+  //Set the pin as output
+  pinMode(CS_CAM, OUTPUT);
+  
+  // initialize SPI:
+  SPI.begin();
+  delay(1000);
+
+  //Check if the ArduCAM SPI bus is OK
+  myCam.write_reg(ARDUCHIP_TEST1, 0x55);
+  delay(1000);
+  temp = myCam.read_reg(ARDUCHIP_TEST1);
+
+  myCam.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+  myCam.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+  while ((vid != 0x56) || (pid != 0x42) || (temp != 0x55)) {
+    myCam.write_reg(ARDUCHIP_TEST1, 0x55);
+    delay(3000);
+    temp = myCam.read_reg(ARDUCHIP_TEST1);
+    if (temp != 0x55) {
+      Serial.println("SPI1 interface Error!");
+    } else {
+      Serial.println("SPI1 OK");
+    }
+
+    // Check if Normal CAM is connected
+    myCam.rdSensorReg16_8(OV5642_CHIPID_HIGH, &vid);
+    myCam.rdSensorReg16_8(OV5642_CHIPID_LOW, &pid);
+
+    if ((vid != 0x56) || (pid != 0x42)) {
+      Serial.println("Can't find OV5642 module!");
+      Serial.println("vid=" + ToString(vid) + " - pid=" + ToString(pid));
+    } else {
+      delay(1000);
+    }
+  }
+  // Serial.println("CAM1 detected.");
+  // Configure JPEG format
+  myCam.set_format(JPEG);
+  myCam.InitCAM();
+
+  //Setting the quality of image at minimum
+  myCam.OV5642_set_JPEG_size(IMAGE_QUALITY);
+  myCam.set_bit(ARDUCHIP_TIM, VSYNC_LEVEL_MASK);
+  myCam.set_bit(ARDUCHIP_GPIO, GPIO_PWDN_MASK); //enable low power
+  myCam.clear_fifo_flag();
+  myCam.write_reg(ARDUCHIP_FRAMES, 0x00);
+  Serial.println("Setup OK!");
+}
+
+void loop() {
+  //Check if it is time to make a pic
+  if (secondOk >= TIME_TO_PIC) {
+    secondOk = 0;
+    ledOn(LED_DISTANCE_NO);
+    ledOn(LED_DISTANCE_OK);
+    capture_normalImage();
+    ledOff(LED_DISTANCE_NO);
+    ledOff(LED_DISTANCE_OK);
+    ledKo_ON = false;
+    ledOk_ON = false;
+  }
+
+  //check the status of the object
+  if (inRange(RANGE_MIN, RANGE_MAX)) {
+    //In range!
+    if ( ! (!ledKo_ON && ledOk_ON) ) {
+      ledOff(LED_DISTANCE_NO);
+      ledKo_ON = false;
+      
+      ledOn(LED_DISTANCE_OK);
+      ledOk_ON = true;
+      Serial.println("Switch OK");
+    }
+    
+    secondOk++;
+  } else {
+    //Not in range
+    if ( ! (ledKo_ON && !ledOk_ON) ) {
+      ledOff(LED_DISTANCE_OK);
+      ledOk_ON = false;
+      
+      ledOn(LED_DISTANCE_NO);
+      ledKo_ON = true;
+      Serial.println("Switch KO");
+    }
+    secondOk = 0;
+  }
+
+  //Wait a second before next measeure 
+  if (secondOk != 0) {
+    delay(SECOND_PAUSE);
+  }
+  
+}
+
+void test_led() {
+   ledOn(LED_DISTANCE_NO);
+   delay(500);
+   ledOn(LED_DISTANCE_OK);
+   delay(500);
+   ledOn(LED_UNSORTED);
+   delay(500);
+   ledOn(LED_PLASTIC);
+   delay(500);
+   ledOn(LED_PAPER);
+   delay(500);
+   
+   ledOff(LED_DISTANCE_NO);
+   delay(500);
+   ledOff(LED_DISTANCE_OK);
+   delay(500);
+   ledOff(LED_UNSORTED);
+   delay(500);
+   ledOff(LED_PLASTIC);
+   delay(500);
+   ledOff(LED_PAPER);
+   delay(500);
+   
+}
+
+bool inRange(int min, int max) {
+  //Min must be the smaller value
+  if (min > max) {
+    int temp = min;
+    min = max;
+    max = temp;
+  }
+  int dist = getDistance();
+  Serial.println("Distance: "+String(dist));
+  bool in_range = (dist <=max && dist >= min);
+  return in_range;
+}
+
+void capture_normalImage() {
+  uint64_t temp = 0;
+  uint64_t temp_last = 0;
+  uint64_t start_capture = 0;
+  
+  //Power up Camera
+  myCam.clear_bit(ARDUCHIP_GPIO, GPIO_PWDN_MASK);
+  delay(500);
+  myCam.flush_fifo();
+  myCam.clear_fifo_flag();
+  myCam.start_capture();
+  while (!myCam.get_bit(ARDUCHIP_TRIG, CAP_DONE_MASK));
+  myCam.set_bit(ARDUCHIP_GPIO, GPIO_PWDN_MASK); //enable low power
+
+  Serial.println("Normal Capture Done!");
+  read_fifo_burst(myCam, "NORM");
+
+  //Clear the capture done flag
+  myCam.clear_fifo_flag();
+  //delay(500);
+}
+
+/*
+   Leggo l'immagine dalla memoria
+*/
+uint8_t read_fifo_burst(ArduCAM myCAM, String CAM_NAME) {
+  //Two variable to store last read byte
+  uint64_t temp = 0;
+  uint64_t temp_last = 0;
+  //Counter used to measure the size of the image
+  int image_len = 0;
+  byte buf[BUFFER];
+  byte small_buf[SMALL_BUF];
+  int buf_index = 0;
+  size_t length = myCAM.read_fifo_length();
+  Serial.println("Expected image of: " + String(length) + "bytes");
+  //Read length of image captured
+  if (length >= 524288) {
+    //512 kb -> Too big image
+    Serial.println("Not found the end.");
+    return 0;
+  }
+
+  //get the image
+  Serial.print("Storing image...");
+  myCAM.CS_LOW();
+  delay(500);
+  myCAM.set_fifo_burst();
+  while ( (temp != 0xD9) | (temp_last != 0xFF)) {
+    temp_last = temp;
+    temp = SPI.transfer(0x00);
+
+    //Print the read byte
+    if (image_len == 0 || image_len == 1) {
+      Serial.println("Byte #" + String(image_len) + ": " + ToString(temp));
+    }
+
+    //Write image data to buffer if not full
+    if (image_len < BUFFER) {
+      buf[image_len] = temp;
+      image_len++;
+    } else {
+      Serial.println("ERROR: Buffer too small!");
+      break;
+    }
+  }
+  Serial.println("DONE!");
+
+  //connect to wifi
+  setup_wifi();
+
+  // Prepare request
+  String start_request = "";
+  String end_request = "";
+  start_request = start_request + "-----------------------------172051402316064980891449920408\r\nContent-Disposition: form-data; name=\"bin_id\"\r\n\r\n" + String(getId()) + "\r\n" +
+                  "-----------------------------172051402316064980891449920408\r\nContent-Disposition: form-data; name=\"bin_image\"; filename=\"Schermata123.jpg\"\r\nContent-Type: image/jpeg\r\nContent-Transfer-Encoding: binary\r\n\r\n";
+  end_request = end_request + "\r\n" + "-----------------------------172051402316064980891449920408--" + "\r\n";
+  //calculate the extra length of the packet
+  long extra_length;
+  extra_length = start_request.length() + end_request.length();
+  long len = image_len + extra_length;
+
+  Serial.print("Starting connection to server...");
+
+  client.connect(host, 80);
+  Serial.println("OK");
+  client.print("POST ");
+  client.print(String(api_page));
+  client.print(" HTTP/1.1\r\n");
+  client.print("Host: " + String(host) + "\r\n");
+  client.print("Content-Type: multipart/form-data; boundary=---------------------------172051402316064980891449920408\r\n");
+  client.print("Content-Length: " + String(len) + "\r\n");
+  client.print("\r\n");
+  //Write the request body
+  client.print(start_request);
+  //Read the image in chunks, store into a small buff and send it
+  for (int i = 0; i < image_len; i++, buf_index++) {
+    //Buff full - Send
+    if (buf_index == SMALL_BUF) {
+      //restart buffer count
+      buf_index = 0;
+      //Send the whole buffer
+      client.write(small_buf, SMALL_BUF);
+      //Serial.println("Buffer sent!");
+    }
+    //Move value to buffer
+    small_buf[buf_index] = buf[i];
+  }
+  //send remaining bytes
+  if (buf_index > 0) {
+    client.write(small_buf, buf_index);
+  }
+
+  client.print(end_request);
+  Serial.println("Transmission over");
+
+
+  String response = getResponse();
+  if (response == "UNSORTED") {
+      blinkLed(LED_UNSORTED);
+  } else if (response == "PLASTIC" || response == "ALUMINIUM") {
+      blinkLed(LED_PLASTIC);
+  } else if (response == "PAPER") {
+      blinkLed(LED_PAPER);
+  } else {
+      Serial.println("Cannot recognise the string");
+  }
+  Serial.println("Stringa letta: "+response);
+  client.stop();
+  WiFi.disconnect();
+  WiFi.end();
+}
+
+String getResponse(){
+  String response = "";
+  bool startFound = false;
+  bool endFound = false;
+  char c;
+  while (client.connected()) {
+    while (client.available()) {
+      // Read answer
+      c = client.read();
+       //Serial.print(c);
+      if ( !startFound && c == '%') {
+        //Starting char found
+        startFound = true;
+      } else if (startFound && !endFound && c != '%') {
+        //Found response
+        response = response + c;
+      } else if (startFound && !endFound && c == '%') {
+        endFound = true;
+        return response;
+      } else if (startFound && endFound) {
+        //Should never be here
+        Serial.println("Should never be here");
+      }
+    }
+  }
+  return response;
+}
+
+bool string_found(char read_char, char* string) {
+  bool found = false;
+  int strlen = String(string).length();
+  //Serial.println("String base lunga: "+String(strlen));
+  //Add the last read char
+  //Serial.print("Ho letto: ");
+  for (int i = 0; i < strlen; i++) {
+    int pos = strlen-i-1;
+   // Serial.print(string[pos]);
+    if (pos == 0) {
+      string[pos] = read_char;
+    } else {
+      //Move forward last char
+      string[pos] = string[pos-1];
+    }
+  }
+ // Serial.println(" - FINE!");
+  //Now compare the new string
+  if (strcmp (string, BIN_STRING) == 0) {
+    Serial.println("Stringa trovata!");
+    found = true;
+  } else if (read_char == '\n') {
+     return true;
+  }
+  return found;
+}
+
+void setup_wifi() {
+  delay(10);
+  // We start by connecting to a WiFi network
+  Serial.print("Connecting to ");
+  Serial.println(wifi_ssid);
+
+  WiFi.begin(wifi_ssid, wifi_password);
+
+  long start1 = millis();
+  long start2 = millis();
+  long elapsed = millis() - start1;
+  while (WiFi.status() != WL_CONNECTED) {
+    elapsed = millis() - start2;
+    if (elapsed > 500) {
+      start2 = millis();
+      Serial.print(".");
+    }
+    if (start2 - start1 > 5000) {
+      Serial.println("Retry connection");
+      start1 = start2;
+      WiFi.disconnect();
+      WiFi.begin(wifi_ssid, wifi_password);
+    }
+  }
+  Serial.println("Connected");
+}
+
+String ToString(uint64_t x)
+{
+  boolean flag = false; // For preventing string return like this 0000123, with a lot of zeros in front.
+  String str = "";      // Start with an empty string.
+  uint64_t y = 10000000000000000000;
+  int res;
+  if (x == 0)  // if x = 0 and this is not testet, then function return a empty string.
+  {
+    str = "0";
+    return str;  // or return "0";
+  }
+  while (y > 0)
+  {
+    res = (int)(x / y);
+    if (res > 0)  // Wait for res > 0, then start adding to string.
+      flag = true;
+    if (flag == true)
+      str = str + String(res);
+    x = x - (y * (uint64_t)res);  // Subtract res times * y from x
+    y = y / 10;                   // Reducer y with 10
+  }
+  return str;
+}
+byte ComBuffer[12];
+int ClearComBuffer()
+{ // clear com buffer for old data
+  for (int i = 0; i < 12; i++)
+  {
+    ComBuffer[i] = 0;
+  }
+  return -1;
+}
+
+/*
+   Get the unique id of the board using the MAC address
+*/
+long getId() {
+  long id = 0;
+  byte mac[6];
+  WiFi.macAddress(mac);
+  const int digits = 3;
+  for (int i = 0; i < digits; i++) {
+    int value = int(mac[i]);
+    id += value * pow(10, i * digits);
+  }
+  return id;
+}
+
+void blinkLed(int led_num) {
+   //Setup led
+   ledOn(led_num);
+   delay(5000);
+   ledOff(led_num);
+}
+
+void ledOn(int led_num) {
+   pinMode(led_num, OUTPUT);
+   //Turn on
+   digitalWrite(led_num, HIGH);
+}
+
+void ledOff(int led_num) {
+   pinMode(led_num, OUTPUT);
+   //Turn on
+   digitalWrite(led_num, LOW);
+}
+
+long getDistance() {
+  //Inizialize pin
+  pinMode(TRIG_PIN, OUTPUT);
+  //Echo
+  pinMode(ECHO_PIN, INPUT);
+
+  //Capture value
+  long duration, distance;
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+
+  digitalWrite(TRIG_PIN, LOW);
+  duration = pulseIn(ECHO_PIN, HIGH);
+  distance = (duration / 2) / 29.1;
+  return distance;
+}
